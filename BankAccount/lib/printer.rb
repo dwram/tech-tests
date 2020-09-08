4c740ef43ae691bfb8fa6318328b0516dd0543b3
Daniel M <danielim@hotmail.co.uk>
2020-09-08 11:26:30 +0100

    Refactored printer transaction logic and removed complexity from transaction class

--- BankAccount/lib/printer.rb
+++ BankAccount/lib/printer.rb
@@ -1,24 +1,20 @@
 # Handles statement output to User
 class Printer
 
-  @beginning_balance = 0
-  @after_transaction = 0
-
   def self.print(statement, balance)
     @current_balance = balance
     puts 'DATE || CREDIT || DEBIT || BALANCE'
     statement.sort.reverse.to_h.map do |_, transaction|
-      statement_output(transaction, balance)
+      statement_output(transaction)
     end
   end
 
   private
 
-  def self.statement_output(transaction, balance)
+  def self.statement_output(transaction)
     credit = '%.2f' % transaction[0].credit if transaction[0].credit
     debit = '%.2f' % transaction[0].debit if transaction[0].debit
-    @persisted_transaction_value = (@beginning_balance + transaction[0].value)
-    @current_balance += -@persisted_transaction_value
+    @current_balance += -transaction[0].value
     puts "#{transaction[0].date} || #{credit} || #{debit} || #{'%.2f' % (@current_balance + transaction[0].value) } "
   end
 
