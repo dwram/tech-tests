2e1551d37993a05ea0a21fae98568067e2c8d92d
Daniel M <danielim@hotmail.co.uk>
2020-09-11 10:38:13 +0100

    Printer spec test shows formatting

--- BankAccount/spec/printer_spec.rb
+++ BankAccount/spec/printer_spec.rb
@@ -10,7 +10,7 @@ describe Printer do
       2 => [withdraw]
     }
     output_statement = Printer.print(statement, 0)
-    expect(output_statement.first).to include(withdraw.date)
-    expect(output_statement[1]).to include(deposit.date)
+    expect(output_statement.first).to eq('11/09/2020 ||  || 100.00 || 0.00')
+    expect(output_statement[1]).to eq('10/09/2020 || 100.00 ||  || 100.00')
   end
 end
